d9daf80d804eef71c2c8dd3a129b05f3cfdfd597
nastysaaa <115701813+nastysaaa@users.noreply.github.com>
2024-12-11 12:45:07 +0200

    99

--- static/script.js
+++ static/script.js
@@ -11,6 +11,59 @@ document.addEventListener("DOMContentLoaded", ()=> {
         //notirīt tabulu
         tableBody.innerHTML = "";
         let totalTemp = 0;
+        data.forEach(entry => {
+            const avgTemp = (entry.min_temp + entry.max_temp) / 2;
+            totalTemp += avgTemp;
+            //Veido tabulas rindas ar ierakstiem
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${entry.date}</td>
+                <td>${entry.min_temp}</td>
+                <td>${entry.max_temp}</td>
+                <td>${avgTemp.toFixed(2)}</td>
+                `;
+            tableBody.appendChild(row);
+        });
+        //Aprēkina visu dienu videjo temp
+        const avgTemp = data.lenght > 0 ? totalTemp / data.lenght : 0;
+        avgTempElement.textContent = `Vidēja temperatūra visām dienām:
+        ${avgTemp.toFixed(2)}`;
 
-    }
+
+    };
+    //Forma datu nosūtišanai
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const date = document.getElementById('date').value;
+        const minTemp = document.getElementById('min_temp').value;
+        const maxTemp = document.getElementById('max_temp').value;
+        //Vai visi lauki ievadīti 
+        if(!date || !minTemp || !maxTemp) {
+            alert("Visi lauki ir obligāti!")
+            return;
+        }
+        const payload = {
+            date: date,
+            min_temp: parseFloat(minTemp),
+            max_temp: parseFloat(maxTemp),
+        };
+        const response = await fetch(apiUrl,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+        });
+        if (response.ok) {
+            form.reset();
+            loadTable();
+        }else {
+            const error = await response.json();
+            alert(error.error || "Kļūda saglabajot datus!");
+
+        }
+        
+    });    
+    //Ielādē tabulu palaižot
+    loadTable();
 })
